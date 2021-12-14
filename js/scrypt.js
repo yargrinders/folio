8b27fe72991c93a1ee5924be5f9f0c00221280b0
yargrinders <yargrindersp20@gmail.com>
2021-12-14 13:42:33 +0200

    Slick / done

--- js/scrypt.js
+++ js/scrypt.js
@@ -40,33 +40,50 @@ $(document).ready(function(){
   $('.slider').slick({
       arrows:true,
       dots:true,
-      adaptiveHeight:true,
-      slidesToShow: 3,
+      adaptiveHeight:false,
+      slidesToShow: 4,
       slidesToScroll: 1,
-      autoplay:false,
+      autoplay:true,
       autoplaySpeed:3500,
       responsive: [
           {
-            breakpoint: 1024,
+            breakpoint: 1170,
             settings: {
               slidesToShow: 3,
               slidesToScroll: 3,
               infinite: true,
-              dots: true
+              dots: true,
+              arrows:true
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows:true
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows:false
             }
           },
           {
             breakpoint: 480,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              arrows:false
             }
           }
       ]
