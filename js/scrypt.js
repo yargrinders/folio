5c0d82f308c3e3bcc52a88915da7f3c435ca0bf3
yargrinders <yargrindersp20@gmail.com>
2021-12-14 09:37:23 +0200

    Slick v1

--- js/scrypt.js
+++ js/scrypt.js
@@ -36,10 +36,39 @@ for (let anchor of anchors) {
   })
 }
 
-$(".center").slick({
-  dots: true,
-  infinite: true,
-  centerMode: true,
-  slidesToShow: 5,
-  slidesToScroll: 3
+$(document).ready(function(){
+  $('.slider').slick({
+      arrows:true,
+      dots:true,
+      adaptiveHeight:true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay:false,
+      autoplaySpeed:3500,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+      ]
+  });
 });
